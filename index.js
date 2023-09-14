96db0dc2e426872aa01ca1701135a233deefa6b0
nlaureta <73408370+nlaureta@users.noreply.github.com>
2023-09-13 21:03:20 -0700

    Added controls (in progress).
    Slider to change square size. Color picker but does not function yet.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function createGrid(rc) {
     const container = document.querySelector('.grid-container');
     const containerSize = container.clientWidth;
+    const squareSize = containerSize / rc ;
+    container.innerHTML = ''; // Clear the existing grid
     console.log(containerSize);
     for (let i = 0; i < rc; i++) {
         for (let j = 0; j < rc; j++) {
             const square = document.createElement('div');
             square.className = 'square';
-            const squareSize = containerSize / rc ;
+            
             square.style.width = `${squareSize-2}px`;
             square.style.height = `${squareSize-2}px`;
             // square.style.width = `50px`;
@@ -17,13 +19,30 @@ function createGrid(rc) {
 
 }
 
-createGrid(50);
+function draw() {
+    const squares = document.querySelectorAll('.square');
 
-const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => { 
+        square.addEventListener('mouseover', function () {
+            // if (e.type == "mousedown") {
+            //     //code triggers on hold
 
-squares.forEach((square) => { 
-    square.addEventListener('mouseover', function () {
-        square.style.backgroundColor = 'black';
-        console.log('hovering');
+            //     console.log("hold");
+            // }
+            square.style.backgroundColor = 'black';
+            console.log('hovering');
+        });
     });
-});
+}
+
+let squareSize = document.querySelector('.slider');
+
+squareSize.addEventListener('input', function() {
+    createGrid(this.value);
+    draw();
+ 
+
+})
+createGrid(squareSize.value);
+
+draw();
