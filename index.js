b45a624fb55b9c1ff6a4efe490c97dc6b7dcdb7c
nlaureta <73408370+nlaureta@users.noreply.github.com>
2023-09-15 22:07:22 -0700

    Added erase slider, color functions, and more styling
    Erase slider to function like the Etch a Sketch Doodle. Button to choose color or randomize color. Styled to look more like Etch a Sketch Doodle.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function createGrid(rc) {
     const container = document.querySelector('.grid-container');
     const containerSize = container.clientWidth;
-    const squareSize = containerSize / rc ;
-    container.innerHTML = ''; // Clear the existing grid
+    const squareSize = containerSize / rc;
+
+    container.innerHTML = '';
     console.log(containerSize);
     for (let i = 0; i < rc; i++) {
         for (let j = 0; j < rc; j++) {
             const square = document.createElement('div');
             square.className = 'square';
-            
-            square.style.width = `${squareSize-2}px`;
-            square.style.height = `${squareSize-2}px`;
+
+            square.style.width = `${squareSize - 2}px`;
+            square.style.height = `${squareSize - 2}px`;
             // square.style.width = `50px`;
             // square.style.height = '50px';
             container.appendChild(square);
@@ -19,30 +20,55 @@ function createGrid(rc) {
 
 }
 
+function toggleButton(button) {
+    if (button.value === "Off") {
+        button.value = "On"
+        console.log("on")
+    } else {
+        console.log("off");
+        button.value = "Off"
+    }
+}
+
 function draw() {
     const squares = document.querySelectorAll('.square');
+    const squareColor = document.querySelector('#colorPicker');
+    const pencilTip = document.querySelector('.pencil-tip');
 
-    squares.forEach((square) => { 
+    let randomColorToggle = document.querySelector('#randomizeColor');
+
+    squares.forEach((square) => {
         square.addEventListener('mouseover', function () {
-            // if (e.type == "mousedown") {
-            //     //code triggers on hold
+            const randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
-            //     console.log("hold");
-            // }
-            square.style.backgroundColor = 'black';
+            if (randomColorToggle.value === "Off") {
+                square.style.backgroundColor = squareColor.value;
+                squareColor.disabled = false;
+                pencilTip.style.borderTopColor = squareColor.value;
+            } else if (randomColorToggle.value === "On") {
+                square.style.backgroundColor = "#" + randomColor;
+                squareColor.disabled = true;
+                pencilTip.style.borderTopColor = "#" + randomColor;
+            }
+
             console.log('hovering');
         });
     });
 }
 
 let squareSize = document.querySelector('.slider');
-
-squareSize.addEventListener('input', function() {
+squareSize.addEventListener('input', function () {
     createGrid(this.value);
     draw();
- 
+});
 
-})
+const eraseSlider = document.querySelector('.eraseSlider');
+eraseSlider.addEventListener('input', function () {
+    if (eraseSlider.value === "1" || eraseSlider.value === "2") {
+        createGrid(squareSize.value);
+        draw();
+    }
+});
+
 createGrid(squareSize.value);
-
 draw();
